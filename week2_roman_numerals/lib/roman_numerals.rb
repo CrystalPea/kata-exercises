8591860ecb62ccb90fdddc9512e2056e9b241c1e
Pea M. Tyczynska <CrystalPea@github.com>
2016-11-15 09:52:11 +0000

    Double digits done

--- week2_roman_numerals/lib/roman_numerals.rb
+++ week2_roman_numerals/lib/roman_numerals.rb
@@ -1,7 +1,25 @@
 def roman_numerals(number)
-  if number == 5; "V"
-  elsif (number > 5); "V" + ("I" * (number % 5))
-  elsif number % 5 == 4; "IV"
-  else "I" * number
+numeral = ""
+double_digits = ["X", "L", "XC"]
+single_digits = ["I", "V", "IX"]
+
+  if number / 10 > 0
+    if number / 10 == 9
+      numeral += "XC"
+    elsif number / 10 > 5
+      numeral += "L" + ("X" * ((number/10) % 5))
+    elsif number / 10 == 5
+      numeral += "L"
+    elsif number / 10 == 4
+      numeral += "XL"
+    else numeral += "X" * (number / 10)
+    end
+  number -= (number / 10) * 10
+  end
+  if number == 9; numeral += "IX"
+  elsif number == 5; numeral += "V"
+  elsif (number > 5); numeral += "V" + ("I" * (number % 5))
+  elsif number == 4; numeral += "IV"
+  else numeral += "I" * number
   end
 end
--- week2_roman_numerals/spec/roman_numerals_spec.rb
+++ week2_roman_numerals/spec/roman_numerals_spec.rb
@@ -20,4 +20,29 @@ describe 'roman_numerals' do
   it "returns 'VIII' if passed 8 as an argument" do
     expect(roman_numerals(8)).to eq "VIII"
   end
+
+  it "returns 'IX' if passed 9 as an argument" do
+    expect(roman_numerals(9)).to eq "IX"
+  end
+
+  it "returns 'XXI' if passed 21 as an argument" do
+    expect(roman_numerals(21)).to eq "XXI"
+  end
+
+  it "returns 'XLVII' if passed 47 as an argument" do
+    expect(roman_numerals(47)).to eq "XLVII"
+  end
+
+  it "returns 'L' if passed 50 as an argument" do
+    expect(roman_numerals(50)).to eq "L"
+  end
+
+  it "returns 'LXV' if passed 65 as an argument" do
+    expect(roman_numerals(65)).to eq "LXV"
+  end
+
+  it "returns 'XCIV' if passed 94 as an argument" do
+    expect(roman_numerals(94)).to eq "XCIV"
+  end
+
 end
